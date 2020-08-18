772032e779260237c029fe3d4039d4bd841cbbcf
Thiago <thiago.nascimento99@hotmail.com>
2020-08-17 22:49:17 -0300

    Minor adjustments

--- scripts/insertIframe.js
+++ scripts/insertIframe.js
@@ -1,7 +1,7 @@
 (function(){
     let ifr = document.createElement('iframe');
-    ifr.height = '800px';
-    ifr.width = '600px';
+    ifr.height = '50px';
+    ifr.width = '50px';
     ifr.src = top.window.location.href;
     document.body.appendChild(ifr);
 })();
