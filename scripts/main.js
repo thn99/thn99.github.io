3e6c188d7da0b60be2fce2c8cfce84cfb149c3e2
Thiago Henrique <38474017+thn99@users.noreply.github.com>
2021-05-05 09:14:53 -0300

    Changed GTM ID

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@
     j.async = true;
     j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
     f.parentNode.insertBefore(j, f);
-  })(window, document, "script", "dataLayer", "GTM-KVRTNC8");
+  })(window, document, "script", "dataLayer", "GTM-N3G5W8J");
 })();
 
 window.purchaseItems = window.purchaseItems || [];
